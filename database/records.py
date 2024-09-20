c4c441350a5ed0910bca91bee1f31935c26c5046
KxxDD <29157441+rlaehd62@users.noreply.github.com>
2024-09-20 22:09:13 +0900

    feat: add functions inserting records

---
+++ database/records.py
@@ -0,0 +1,29 @@
+from database.connection import get_connection
+from database.query_constant import FETCH_RECORDS, FETCH_ONE_RECORDS
+from pymysql.cursors import DictCursor
+
+def fetch_records_one(id: int) -> tuple:
+    with get_connection() as connection:
+        try:
+            with connection.cursor(DictCursor) as cursor:
+                cursor.execute(FETCH_ONE_RECORDS, id)
+                return cursor.fetchone()
+        
+        except Exception as e:
+            print(f'Error occurred while fetching records: {e}')
+            return {}
+        
+
+def fetch_records(idList: list) -> tuple:
+    with get_connection() as connection:
+        try:
+            with connection.cursor(DictCursor) as cursor:
+                cursor.execute(FETCH_RECORDS(len(idList)), (idList))
+                return list(cursor)
+        
+        except Exception as e:
+            print(f'Error occurred while fetching records: {e}')
+            return ()
+        
+        finally:
+            connection.close()
