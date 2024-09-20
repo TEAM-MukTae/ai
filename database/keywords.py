8ac80bd3d209176c5343fe6218823ceabd32b37a
KxxDD <29157441+rlaehd62@users.noreply.github.com>
2024-09-20 22:09:30 +0900

    feat: add functions inserting keywords

---
+++ database/keywords.py
@@ -0,0 +1,18 @@
+from database.connection import get_connection
+from database.query_constant import INSERT_KEYWORDS
+
+def insert_keywords(rs_id: int, names: list) -> tuple:
+    with get_connection() as connection:
+        try:
+            with connection.cursor() as cursor:
+                print(INSERT_KEYWORDS(rs_id, names))
+                cursor.execute(INSERT_KEYWORDS(rs_id, names), (names))
+                connection.commit()
+            
+        except Exception as e:
+            print(f'Error occurred while fetching records: {e}')
+            connection.rollback()
+            return -1
+            
+        finally:
+            return rs_id
